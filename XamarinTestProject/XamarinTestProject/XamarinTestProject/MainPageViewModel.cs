f857ec69ef7c43bc613e76e0d170c678dd9872c4
JeffACate <Jeff.A.Cate@gmail.com>
2021-11-06 19:52:35 -0500

    Created main page view model

---
+++ XamarinTestProject/XamarinTestProject/XamarinTestProject/MainPageViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XamarinTestProject
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public MainPageViewModel()
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        int count;
+        public int Count
+        {
+            get => count;
+            set
+            {
+                count = value;
+
+                var args = new PropertyChangedEventArgs(nameof(Count));
+
+                PropertyChanged?.Invoke(this, args);
+            }
+        }
+        Command AddCommand { get; }
+        Command ResetCommand { get; }
+        Command DeleteCommand { get; }
+    }
+}
